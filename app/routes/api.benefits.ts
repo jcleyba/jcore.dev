95aca96d01657a01b5641c3a761f5961fa9e896e
Juan Cruz Leyba <juan.leyba@getcenter.com>
2025-01-06 11:49:49 -0300

    test endpoints 2

--- app/routes/api.benefits.ts
+++ app/routes/api.benefits.ts
@@ -3,11 +3,12 @@ import { json } from "@remix-run/node";
 
 export const loader = async () => {
   const users = Array.from({ length: 15000 }, (_, index) => ({
-    quantity: faker.number.int({ min: 1, max: 10 }),
-    userId: index,
-    name: faker.commerce.productName(),
-    deliveredBy: faker.person.fullName(),
-    deliveredAt: faker.date.recent({ days: 30 }).toISOString(),
+    id: faker.string.uuid(),
+    cantidad: faker.number.int({ min: 1, max: 10 }),
+    user_id: index,
+    beneficio: faker.commerce.productName(),
+    entregado_por: faker.person.fullName(),
+    entregado_el: faker.date.recent({ days: 30 }).toISOString(),
   }));
 
   return json({ users });
--- app/routes/api.users.ts
+++ app/routes/api.users.ts
@@ -3,15 +3,11 @@ import { json } from "@remix-run/node";
 
 export const loader = async () => {
   const users = Array.from({ length: 15000 }, (_, index) => ({
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    personalId: index,
-    affiliateStatus: faker.helpers.arrayElement([
-      "active",
-      "inactive",
-      "pending",
-    ]),
-    updatedAt: faker.date.recent({ days: 30 }).toISOString(),
+    id: index,
+    nombre_completo: faker.person.fullName(),
+    cuit: faker.string.uuid(),
+    afiliado: Math.random() > 0.5 ? "Si" : "No",
+    created_at: faker.date.recent({ days: 30 }).toISOString(),
   }));
 
   return json({ users });
