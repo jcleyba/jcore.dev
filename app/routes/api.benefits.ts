a2735318991c675d6783928d6a0d357ead04ea49
Juan Cruz Leyba <juan.leyba@getcenter.com>
2025-01-06 12:58:22 -0300

    test endpoints 4

--- app/routes/api.benefits.ts
+++ app/routes/api.benefits.ts
@@ -2,10 +2,10 @@ import { faker } from "@faker-js/faker";
 import { json } from "@remix-run/node";
 
 export const loader = async () => {
-  const benefits = Array.from({ length: 15000 }, (_, index) => ({
+  const benefits = Array.from({ length: 15000 }, () => ({
     id: faker.string.uuid(),
     cantidad: faker.number.int({ min: 1, max: 10 }),
-    user_id: index.toString(),
+    user_id: faker.number.int({ min: 10000000, max: 10015000 }),
     beneficio: faker.commerce.productName(),
     entregado_por: faker.person.fullName(),
     entregado_el: faker.date.recent({ days: 30 }).toISOString(),
--- app/routes/api.users.ts
+++ app/routes/api.users.ts
@@ -2,8 +2,8 @@ import { faker } from "@faker-js/faker";
 import { json } from "@remix-run/node";
 
 export const loader = async () => {
-  const users = Array.from({ length: 15000 }, (_, index) => ({
-    id: index.toString(),
+  const users = Array.from({ length: 15000 }, () => ({
+    id: faker.number.int({ min: 10000000, max: 10015000 }),
     nombre_completo: faker.person.fullName(),
     cuit: faker.string.uuid(),
     afiliado: Math.random() > 0.5 ? "Si" : "No",
