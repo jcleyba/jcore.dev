c6797ab9338a9bf448bd3f8c65a7e83da31c66f8
Juan Cruz Leyba <juan.leyba@getcenter.com>
2025-01-06 13:03:38 -0300

    test endpoints 4

--- app/routes/api.benefits.ts
+++ app/routes/api.benefits.ts
@@ -5,7 +5,7 @@ export const loader = async () => {
   const benefits = Array.from({ length: 15000 }, () => ({
     id: faker.string.uuid(),
     cantidad: faker.number.int({ min: 1, max: 10 }),
-    user_id: faker.number.int({ min: 10000000, max: 10015000 }),
+    user_id: faker.number.int({ min: 10000000, max: 10015000 }).toString(),
     beneficio: faker.commerce.productName(),
     entregado_por: faker.person.fullName(),
     entregado_el: faker.date.recent({ days: 30 }).toISOString(),
